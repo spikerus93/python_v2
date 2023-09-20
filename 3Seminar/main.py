5be0a0e89573f2f32a3f40c3b694908d0cb48333
master <spikerus93@rambler.ru>
2023-09-20 11:39:30 +0300

    решение задач на 3-ем семинаре

---
+++ 3Seminar/main.py
@@ -0,0 +1,37 @@
+# array - массив содержит только один тип данных
+# list - список может содержать разные типы данных
+# example = [1, 2.0, "IVAN", True]
+#          0   1     2      3
+# import array <-
+# import numpy <- pip install numpy
+# print(*example)
+# set_1 = set()
+# for i in 3, 56, 13, 26, 18, 9, 23:
+#     set_1.add(i)
+
+# print(set_1)
+# print(int("07"))
+
+# list_1 = [1, 1, 2, 0, -1, 3, 4, 4]
+# #                              -1
+# print(list_1[-1])
+# print(tuple(list_1))
+# print(len(set(list_1)))
+# print(list((4, 2, 3, 1, 5)))
+# # len -> Length
+# for i in range(-1, -len(list_1), -1):
+#     print(list_1[i])
+
+# Dictionary
+# data = {}
+# data_second = dict()
+# # key: value
+# data['Кирилл'] = 23
+# data['Марина'] = 20
+# # print(data["Кирилл"])
+# # print(list(data.keys()))
+# # print(list(data.values()))
+# for i in data: # <-> data.keys()
+#     print(i)
+# for i in data.values():
+#     print(i)
