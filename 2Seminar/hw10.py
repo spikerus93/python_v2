8716859d8fc19a07e4da5e4b989669e8607ada2f
master <spikerus93@rambler.ru>
2023-09-20 11:29:01 +0300

    доп. решение по 10 задаче *(монетки)

--- 2Seminar/hw10.py
+++ 2Seminar/hw10.py
@@ -23,3 +23,19 @@ if x1 < x2:
     print(f'Надо перевернуть {x1} монеток')
 else:
     print(f'Надо перевернуть {x2} монеток')
+
+# Доп. решение.
+# Task 10
+# n = int(input("Введите кол-во монет: "))
+# count_zero = 0
+# count_one = 0
+# for i in range(n):
+#     x = int(input("Введите сторону монеты(1 или 0): "))
+#     if x == 1:
+#         count_one += 1
+#     else:
+#         count_zero += 1
+# if count_zero > count_one:
+#     print(count_one)
+# else:
+#     print(count_zero)
