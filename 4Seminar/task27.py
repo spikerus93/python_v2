2bbc66f13e8a180774b7a8a59232375f747cd592
master <spikerus93@rambler.ru>
2023-09-21 10:54:12 +0300

    решение задач на 4-ом семинаре

---
+++ 4Seminar/task27.py
@@ -0,0 +1,21 @@
+# Задача №27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+# Output: 13
+
+s = '''She sells sea shells on the sea shore The shells
+that she sells are sea shells I'm sure.So if she sells sea
+shells on the sea shore I'm sure that the shells are sea
+shore shells'''.lower()
+# s = set(s.split())
+# print(len(s))
+
+print(len(set(s.lower().split())))
+#print(len(set(input("Input text: ").lower().split())))
