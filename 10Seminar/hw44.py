1a1d1f5dd4ad6726b3abf44416a06b98e04ec7a7
master <spikerus93@rambler.ru>
2023-10-12 23:11:57 +0300

    Домашнее задание №44 по 10-ому Семинару

---
+++ 10Seminar/hw44.py
@@ -0,0 +1,20 @@
+# Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца.
+# Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
+
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+data.head()
+
+uniq_values = data['whoAmI'].unique()  # Получаем уникальные значения
+one_hot = pd.DataFrame()  # Создаем новый Датафрейм
+
+# Поиск уникальных значений для добавления в новый Датафрейм с столбцом Значения.
+for value in uniq_values:
+    one_hot[value] = (data['whoAmI'] == value).astype(int)
+one_hot.head()
+print(one_hot)
